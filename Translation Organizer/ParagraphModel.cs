abfa13280cd84e9daf1a2e7481e83546616dcab8
Vicky <nakamanoaki@gmail.com>
2024-06-20 21:24:32 -0400

    Made a paragraph model class that contains what the collections of jp, romaji, english sentences that make up a paragraph.

---
+++ Translation Organizer/ParagraphModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace Translation_Organizer
+{
+    internal class ParagraphModel : INotifyPropertyChanged
+    {
+        private readonly ObservableCollection<string> jpSentences;
+        private readonly ObservableCollection<string> rmjSentences;
+        private readonly ObservableCollection<string> enSentences;
+
+        public ObservableCollection<string> JpSentences { get { return jpSentences; } }
+        public ObservableCollection<string> RmjSentences { get { return rmjSentences; } }
+        public ObservableCollection<string> EnSentences { get { return enSentences; } }
+
+
+        public ParagraphModel()
+        {
+            jpSentences = new ObservableCollection<string>();
+            jpSentences.CollectionChanged += JpSentences_CollectionChanged;
+            rmjSentences = new ObservableCollection<string>();
+            rmjSentences.CollectionChanged += RmjSentences_CollectionChanged;
+            enSentences = new ObservableCollection<string>();
+            enSentences.CollectionChanged += EnSentences_CollectionChanged;
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if(this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void JpSentences_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            this.NotifyPropertyChanged(nameof(JpSentences));
+        }
+        private void RmjSentences_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            this.NotifyPropertyChanged(nameof(RmjSentences));
+        }
+        private void EnSentences_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            this.NotifyPropertyChanged(nameof(EnSentences));
+        }
+    }
+}
