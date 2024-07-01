88578143b9c8b6a60260cb3da62e3a9f3ac9c1b0
Vicky <nakamanoaki@gmail.com>
2024-07-01 05:39:33 -0400

    Finished the different display tabs and added showing the index next to the selectable paragraph.

---
+++ Translation Organizer/Converters/ParagraphToIndexConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Translation_Organizer
+{
+    internal class ParagraphToIndexConverter:IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var paragraphList = (ObservableCollection<ParagraphModel>)values[0];
+            var paragraph = (ParagraphModel)values[1];
+            if(paragraphList == null || paragraph == null)
+            {
+                return "";
+            }
+
+            int index = paragraphList.IndexOf(paragraph) + 1;
+            return index.ToString();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
